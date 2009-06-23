aef3a1d554388a0ef4252a30b6b6d425d016fb68
Ben Walding <bwalding@rambunctious.local>
2009-06-23 22:29:06 +1000

    Create magical backgroundrb system

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -23,24 +23,59 @@ module SearchHelper
     parse_query
     if not @postgres_search
       @messages = []
-      return render( :template => '/search/index.html.erb' )
+      return render( :template => '/search/index.html.haml' )
     end
     
     if @target
       cache( "search/#{@target.class.name}/#{@target.identifier}/#{@search_key}/#{latest_id}" ) do
+        @worker_key = generate_worker_key()
+        @job_key = generate_worker_key()
+        
+        #@actual_worker_key = MiddleMan.new_worker(:worker => :search_worker)
+                                #:worker_key => @worker_key)
+        @worker = MiddleMan.worker(:search_worker)#, @actual_worker_key)
         case @target
           when Group
-            @messages = search_group(@target, @postgres_search)
+            @worker.enq_search_group(:job_key => @job_key, :args => [ @postgres_search, @target.id ]) 
+            #flash[:warnings] << "Your search has been queued (#{@worker_key})!"
           when List
-            @messages = search_list(@target, @postgres_search)
+            @worker.enq_search_list(:job_key => @job_key, :args => [ @postgres_search, @target.id ]) 
+            #flash[:warnings] << "Your search has been queued (#{@worker_key})!"
         end
 
-        return render( :template => '/search/index.html.erb' )
+        return render( :template => '/search/index' )
       end
     else
       cache( "search/all/0/#{@search_key}/#{latest_id}" ) do
-        @messages = search_all(@postgres_search)
-        return render( :template => '/search/index.html.erb' )
+        @worker.enq_search_all(:args => [ @postgres_search ]) 
+        return render( :template => '/search/index' )
+      end
+    end
+  end
+  
+  def search_results
+    @worker = MiddleMan.worker(:search_worker)#, @worker_key)
+
+    @worker_key = params[:worker_key]
+    @job_key = params[:job_key]
+    @worker_info = @worker.worker_info 
+    
+    puts @worker_info.inspect
+    
+    if request.xhr?
+      message_ids = @worker.ask_result(@job_key + '_results')
+      
+      render :update do |page|
+        if not message_ids.nil?
+          @messages = message_ids.collect { |id| Message.find(id) }
+          content = render( :partial => '/search/search_results', :locals => { :messages => @messages } )
+          page.remove('spinner')
+          page.call('stop_xhr')
+          page.replace_html 'search-results', content
+        else
+          content = render( :partial => '/search/search_status', :locals => { :all_worker_info => MiddleMan.all_worker_info } )
+          page.replace_html 'search-results', content
+        end
       end
     end
   end
@@ -62,19 +97,19 @@ private
     @english_search = @keywords.collect { |k| "<b>#{k}</b>" }.join(' AND ')
     
     logger.trace {"Searching for #{@postgres_search}"}
- end
-
-  def search_list(target, searchterms)
-    return Message.find_by_sql([ "SELECT * FROM sp_search_list(?, to_tsquery('march_config', ?))", target.id, searchterms ])
   end
+
  
- def search_group(target, searchterms)
-   return Message.find_by_sql([ "SELECT * FROM sp_search_group(?, to_tsquery('march_config', ?))", target.id, searchterms ])
- end
- 
- def search_all(searchterms)
-   return Message.find_by_sql([ "SELECT * FROM sp_search_all(to_tsquery('march_config', ?))", searchterms ])
- end
- 
+  def generate_worker_key
+    length = 16
+    bytes = OpenSSL::Random.random_bytes(16 / 2)
+
+    result = ""
+    bytes.each_byte { |b|
+      result << sprintf("%02x", b)  
+    }
+
+    return result
+  end 
   
 end
---
+++ lib/workers/search_worker.rb
@@ -0,0 +1,51 @@
+################################################################################
+#  Copyright 2006-2009 Codehaus Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+################################################################################
+
+class SearchWorker < BackgrounDRb::MetaWorker
+  set_worker_name :search_worker
+  
+  def create(args = nil)
+    # this method is called, when worker is loaded for the first time
+  end
+  
+  def search_list(args)
+    searchterms = args[0]
+    list_id = args[1]
+    result = Message.find_by_sql([ "SELECT * FROM sp_search_list(?, to_tsquery('march_config', ?))", list_id, searchterms ])
+    puts "#{job_key()} search_list complete"
+    cache[job_key() + '_results'] = result.collect { |m| m.id }
+    return result
+  end
+  
+  def search_group(args)
+    searchterms = args[0]
+    group_id = args[1]
+    result = Message.find_by_sql([ "SELECT * FROM sp_search_group(?, to_tsquery('march_config', ?))", group_id, searchterms ])
+    puts "#{job_key()} search_group complete"
+    cache[job_key() + '_results'] = result.collect { |m| m.id }
+    return result
+  end
+  
+  def search_all(args)
+    searchterms = args[0]
+    result = Message.find_by_sql([ "SELECT * FROM sp_search_all(to_tsquery('march_config', ?))", searchterms ])
+    puts "#{job_key()} search_all complete"
+    cache[job_key() + '_results'] = result.collect { |m| m.id }
+    return result
+  end
+  
+end
+
